dabcf59b26dafd08f700384ee42546173e30ccc0
Seang Lim <seanglim5566@outlook.com>
2022-01-15 17:30:20 +0700

    FirstPage Design of Project

--- BudgetExpenseMainWindow.xaml.cs
+++ BudgetExpenseMainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace BudgetExpense
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
