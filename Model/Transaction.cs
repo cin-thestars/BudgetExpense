575c9f0fb17e79a381d7479ff87c8c58e8f50af0
sreng-step <sreng.itstep@gmail.com>
2022-01-17 16:26:40 +0700

    Introduce MVVM

---
+++ Model/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetExpense.Model
+{
+    public class Transaction
+    {
+        public double Incomes { get; set; } = 0;
+        public double Expenses { get; set; } = 0;
+    }
+    public class TransactionList
+    {
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>
+        {
+            new Transaction{Incomes=10, Expenses=0},
+            new Transaction{Incomes=0, Expenses=30},
+            new Transaction{Incomes=50, Expenses=0},
+        };
+        public void LoadData() { }
+        public void SaveData() { }
+    }
+}
---
+++ ViewModel/TransactionViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BudgetExpense.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetExpense.ViewModel
+{
+    public class TransactionViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Transaction> Transactions { get; set; }
+        private TransactionList transactionList = new TransactionList();
+        public TransactionViewModel()
+        {
+            Transactions = new ObservableCollection<Transaction>(transactionList.Transactions);
+        }
+    }
+}
