9c5a2f7b02e3c7d44add181ec152afcdb6475829
Seang Lim <seanglim5566@outlook.com>
2022-01-19 01:03:34 +0700

    Finish all the class of Model and ViewModel in Add WindowForm

--- AddTransaction.xaml.cs
+++ AddTransaction.xaml.cs
@@ -10,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BudgetExpense
 {
     /// <summary>
-    /// Interaction logic for BudgetExpenseMainWindow.xaml
+    /// Interaction logic for AddTransaction.xaml
     /// </summary>
-    public partial class BudgetExpenseMainWindow : Window
+    public partial class AddTransaction : Window
     {
-        public BudgetExpenseMainWindow()
+        public AddTransaction()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Add_NewTransaction(object sender, RoutedEventArgs e)
-        {
-            AddTransaction at = new AddTransaction();
-            at.Show();
         }
     }
 }
---
+++ Model/CategoryIncomeAdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetExpense.Model
+{
+    public class CategoryIncomeAdd
+    {
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+
+    public class CategoryIncome
+    {
+        public List<CategoryIncomeAdd> IncomeModel { get; set; } =
+            new List<CategoryIncomeAdd> 
+            {
+                new CategoryIncomeAdd{Name = "Investment"},
+                new CategoryIncomeAdd{Name = "Salary"},
+                new CategoryIncomeAdd {Name = "Income"},
+                new CategoryIncomeAdd {Name = "Interest"}
+            };
+    }
+}
