ea23bc3a3e7192b6a540c04eab928b394c3c94de
Seang Lim <seanglim5566@outlook.com>
2022-01-19 17:29:30 +0700

    kk

---
+++ Model/ExpData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetExpense.Model
+{
+    public class ExpData
+    {
+        public string Type { get; set; } = "Expense";
+        public string Name { get; set; } = "";
+        public string Category { get; set; } = string.Empty;
+        public double Amount { get; set; } = 0;
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+}
